89e61ac7e843331184d3e3c2d22d279d62c89377
xiongzhiqing <xiongzhiqing@everjiankang.com>
2020-12-09 09:07:33 +0800

    add githu action workflow

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ const MonacaWebpackPlugin = require('monaco-editor-webpack-plugin')
 const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 const isLib = process.env.TYPE === 'lib'
+// const isCI = process.env.CI
 
 module.exports = {
   chainWebpack(config) {
